26abf4b5374d10e167f362efe16478346b384378
Emerson R. S. A <33014935+Emylkk@users.noreply.github.com>
2020-09-13 17:03:55 -0300

    Update RiverRaid.cpp

--- RiverRaid/RiverRaid.cpp
+++ RiverRaid/RiverRaid.cpp
@@ -2,22 +2,28 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include<iostream>
 
-#define janela_altura  400
+#define janela_altura  800
 #define janela_largura 800
 float rotacao = 1;
 float zoom = 1;
-float pontax = 20, pontay = 20, velocidadebola = 10, pontaxPkb = -20, pontayPkb = -20;
-float trans = 1;
+float direcaoX = 20, direcaoY = 20, velocidadebola = 10, pontaxPkb = -20, pontayPkb = -20;
+int translMapa = 0;
 float tx = 0.0, ty = 0.0, xStep = 4, yStep = 4, raiox = 20, raioy = 25;
+int distanciaMapa = -400;
+int x[10]{ 20, 100, 300, 150, 250, 100, 30, 300, 200, 350 };
+
+
 void tela(GLsizei w, GLsizei h);
 void keyboard();
 void display(void);
-void mousemoveu(int x, int y);
+
 void anima(int valor);
 void colisao();
 int main(int argc, char** argv)
 {
+
     glutInit(&argc, argv);  // controla se o sistema operacional tem suporte a janelas.
 
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);  // quantidade de buffer de cores e que o padrao de cores é RGB ou RGBA
@@ -31,7 +37,6 @@ int main(int argc, char** argv)
     glutDisplayFunc(display);
     glutIdleFunc(display);
     glutTimerFunc(velocidadebola, anima, 1);
-    glutPassiveMotionFunc(mousemoveu);
     glutSetCursor(GLUT_CURSOR_CROSSHAIR);
 
     glutMainLoop();
@@ -46,9 +51,6 @@ void anima(int valor) {
 void colisao() {
 
 }
-void mousemoveu(int x, int y) {
-
-}
 void tela(GLsizei w, GLsizei h)
 {
 
@@ -58,12 +60,62 @@ void tela(GLsizei w, GLsizei h)
     gluOrtho2D(0, janela_largura, 0, janela_altura);
     glMatrixMode(GL_PROJECTION);
 }
+static class Mapa {
+public:
+    static void DesenharParede() {
+        Sleep(5);
+
+        glPushMatrix();
+        //Parede Direita
+        
+            glBegin(GL_QUADS);
+            glColor3f(0, 0.8, 0);
+            
+
+            for (size_t i = 0; i < 10; i++)
+            {
+                glVertex2d(x[i], distanciaMapa);//esquerda abaixo
+                glVertex2d(x[i], distanciaMapa+800);//subir na esquerda ↑
+                glVertex2d(400, distanciaMapa + 800);//direita acima
+                glVertex2d(400, distanciaMapa);//direita ↓
+
+                distanciaMapa += 800;
+            }
+            distanciaMapa = -400;
+
+            glEnd();
+
+            //Parede Esquerda
+            glBegin(GL_QUADS);
+            glColor3f(0, 0.8, 0);
+
+
+            for (size_t i = 0; i < 10; i++)
+            {
+                glVertex2d(-x[i], distanciaMapa);//esquerda abaixo
+                glVertex2d(-x[i], distanciaMapa + 800);//subir na esquerda ↑
+                glVertex2d(-400, distanciaMapa + 800);//direita acima
+                glVertex2d(-400, distanciaMapa);//direita ↓
+
+                distanciaMapa += 800;
+            }
+            glEnd();
+
+            glPopMatrix();
+            distanciaMapa = -400;
+            
+    }
+};
+
+
 static class Jogador {
 public:
     static void desenharP1() {
+
         glPushMatrix();//empilha uma matriz
 
-        glTranslatef(pontax, pontay, 0);//realoca a matriz no plano
+        glTranslatef(direcaoX, direcaoY, 0);//realoca a matriz no plano
+
 
         //centro do avião
         glBegin(GL_POLYGON);
@@ -72,7 +124,11 @@ public:
         glVertex2d(-2, 15);
         glVertex2d(2, 15);
         glVertex2d(2, -15);
+        glEnd();
+
         //asa direita inferior
+
+        glBegin(GL_POLYGON);
         glVertex2d(2, -12);
         glVertex2d(6, -12);
         glVertex2d(6, -15);
@@ -81,10 +137,46 @@ public:
         glVertex2d(6, -12);
         glVertex2d(6, -10);
         glVertex2d(2, -10);
-
         glEnd();
-          
-        glPopMatrix();//desempilha matriz pra desenhar apartir da matriz original
+
+
+        //asa esqueda inferior 
+
+        glBegin(GL_POLYGON);
+        glVertex2d(-2, -12);
+        glVertex2d(-6, -12);
+        glVertex2d(-6, -15);
+        glVertex2d(-10, -15);
+        glVertex2d(-10, -12);
+        glVertex2d(-6, -12);
+        glVertex2d(-6, -10);
+        glVertex2d(-2, -10);
+        glEnd();
+
+
+        //asa direita superior 
+        glBegin(GL_POLYGON);
+        glVertex2d(2, 0);
+        glVertex2d(18, -5);
+        glVertex2d(14, 0);
+        glVertex2d(2, 10);
+        glVertex2d(2, 0);
+        glEnd();
+
+
+        //asa esquerda superior 
+        glBegin(GL_POLYGON);
+        glVertex2d(-2, 0);
+        glVertex2d(-18, -5);
+        glVertex2d(-14, 0);
+        glVertex2d(-2, 10);
+        glVertex2d(-2, 0);
+        glEnd();
+
+
+        glPopMatrix();
+
+
 
     }
 
@@ -92,8 +184,10 @@ public:
 void desenhar() {
     Sleep(5);
     Jogador::desenharP1();
-
-
+   translMapa += 1;
+   glTranslatef(0, -translMapa, 0);
+    Mapa::DesenharParede();
+    
     glFlush();
 }
 void display() {
@@ -102,7 +196,7 @@ void display() {
     glLoadIdentity();
 
     glTranslatef(janela_largura / 2, janela_altura / 2, 0.0);
-    glClearColor(0.001f, 0.5f, 0.02f, 1.0f);  // cor do fundo
+    glClearColor(0.00f, 0.0f, 1.0f, 1.0f);  // cor do fundo
     glClear(GL_COLOR_BUFFER_BIT);  // limpa a tela com a cor do fundo
     glViewport(0, 0, janela_largura, janela_altura);
 
@@ -114,23 +208,23 @@ void keyboard()
 {
 
     if (GetAsyncKeyState('W') != 0) {
-        if (pontayPkb < 200) {
-            pontayPkb += 2;
+        if (direcaoY < 384) {
+            direcaoY += 2;
         }
     }
     if (GetAsyncKeyState('A') != 0) {
-        if (pontaxPkb > -300) {
-            pontaxPkb -= 2;
+        if (direcaoX > -380) {
+            direcaoX -= 2;
         }
     }
     if (GetAsyncKeyState('S') != 0) {
-        if (pontayPkb > -200) {
-            pontayPkb -= 2;
+        if (direcaoY > -384) {
+            direcaoY -= 2;
         }
     }
     if (GetAsyncKeyState('D') != 0) {
-        if (pontaxPkb < -20) {
-            pontaxPkb += 2;
+        if (direcaoX < 380) {
+            direcaoX += 2;
         }
     }
     glutPostRedisplay();
